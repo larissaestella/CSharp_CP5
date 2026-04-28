5e9da6729e2eafee878733fc82f2ebfd79db03de
Larissa Estella <larissaestella56@gmail.com>
2026-04-28 16:43:41 -0300

    feat(interfaces): define repository contracts for game and user entities

--- Interfaces/IGameRepository.cs
+++ Interfaces/IGameRepository.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using GameStore.Models;
+using System.Collections.Generic;
+
+namespace GameStore.Interfaces
+{
+    public interface IGameRepository
+    {
+        List<Game> GetAll();
+        Game GetById(int id);
+        void Add(Game game);
+        void Update(Game game);
+        void Delete(int id);
+    }
+}
--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using GameStore.Models;
+
+namespace GameStore.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        User GetByEmail(string email);
+    }
+}
